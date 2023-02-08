fd29a5b8958708918082718cfaf9e6c290320de2
MJSher <90425926+MJSher@users.noreply.github.com>
2023-02-08 13:45:06 -0600

    Update Gradebook.h
    Started the gradebook.h file. Added the private name, category, and points_earned variables. I started on the public individial grade, category grade, and course total methods.

--- Gradebook.h
+++ Gradebook.h
@@ -1,1 +1,18 @@
+#include <vector>
+#include <string>
+#ifndef GRADEBOOK_H
+#define GRADEBOOK_H
+class GradeBook{
+private:
+    std::vector<std::string> catagory;
+    std::vector<int>points_earned;
+    std::vecotr<std::string> name;
 
+public:
+    int individual_grade();
+    int category_grade();
+    int course_total_grade();
+
+};
+
+#endif //GRADEBOOK_H
