068a22e60be79027b05383f670f730b45f09f1fe
andrew56012 <98777690+andrew56012@users.noreply.github.com>
2023-02-08 14:44:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+#include "grade.h"
 
+int main(int argc, char*argv[]) {
+    Grade gradeBook = Grade();
+    gradeBook.getLabs();
+    gradeBook.getHomework();
+    gradeBook.getFinal();
+    gradeBook.getProject();
+    gradeBook.getMEC();
+    gradeBook.calculateGrade();
+}
