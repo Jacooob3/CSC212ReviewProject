6e100336543fe83fa2a530949eb65938d1f69002
ivanabreu0 <102328381+ivanabreu0@users.noreply.github.com>
2023-02-27 14:10:58 -0500

    Update GradeBook.cpp

--- GradeBook.cpp
+++ GradeBook.cpp
@@ -1,25 +1,52 @@
-#include "GradeBook.h"
+#include "gradebook.h"
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include <utility> //for pairs
 
-//constructor
+//default constructor
 GradeBook::GradeBook(){
     std::vector<int> grades;
+    this -> size = 0;
 }
-void GradeBook::GradeBook (std::vector<std::string> category_vector, std::vector<std::string> name_vector, std::vector<int> points_earned_vector){
+
+//ideal constructor of the class. takes in the assignment name, category, and grade
+GradeBook::GradeBook (std::vector<std::string> category_vector, std::vector<std::string> name_vector, std::vector<int> points_earned_vector){
     for (int i = 0; i < category_vector.size(); i++){
         this->category.push_back(category_vector[i]);
         this->name.push_back(name_vector[i]);
         this->points_earned.push_back(points_earned_vector[i]);
     }
+    this -> size = category_vector.size();
+
+}
+//orders all the categories into unique categories, essentially making a list of every unique category
+void GradeBook::order_category() {
+    std::vector<std::string> temp;
+    for (int i = 0; i < category.size(); i++) { //made loop to make unique category vector
+        bool hi = true;
+        for (int j = 0; j < temp.size(); j++) {
+            if (category[i] == temp[j]) {
+                hi = false;
+                break;
+            }
+        }
+        if (hi) {
+            temp.push_back(category[i]);
+        }
+    }
+    for (int i = 0; i < temp.size(); i++) {
+        this->unique_category.push_back(temp[i]);
+    }
+
 }
 
 //methods that allow you to append a value to one of the three vectors
 void GradeBook::category_add_on(std::string value)
 {
     this->category.push_back(value);
+    this -> size += 1;
 }
 void GradeBook::name_add_on(std::string value)
 {
@@ -35,12 +62,12 @@ void GradeBook::points_earned_add_on(int value)
 //an individual project of that category
 int GradeBook::get_assignment_points_total(std::string category_name)
 {
-    const char* category_list[] = {"Lab", "Assignment", "Project1", "Project2", "Exam"};
+    //const char* category_list[] = {"Lab", "Assignment", "Project1", "Project2", "Exam"};
     //grabs an index for the assignment so it can work for the switch statement
     int index;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < unique_category.size(); i++)
     {
-        if (category_list[i] == category_name)
+        if (unique_category[i] == category_name)
         {
             index = i;
         }
@@ -67,6 +94,119 @@ int GradeBook::get_assignment_points_total(std::string category_name)
 //The reason why is because I can reuse code.
 //we will do this using a pair<int><int>. First will be points_earned and second will be points_total
 
+
+//These next three functions print the all the assignment, category, and over all grades respectively
+void GradeBook::printAssignment(std::string file_name){
+    std::vector<std::string> ordered_list;
+    for (int j = 0; j < unique_category.size(); j++) { //Orders all of the given assignments by category
+        for (int i = 0; i < category.size(); i++) {
+            if (category[i] == unique_category[j]) {
+                ordered_list.push_back(name[i]);
+            }
+        }
+    }
+    //looping through to print the grade of all given assignments as a percentage and out of course value
+    if (file_name.size() == 0) { //code to output to terminal
+        std::cout << "Assignment Totals:" << "\n";
+        for (int i = 0; i < size; i++) {
+            std::pair<int, int> temp = individual_grade(ordered_list[i]);
+            std::cout << ordered_list[i] << " " << temp.first << "/" << temp.second << "\t" << "or "
+                      << turn_to_percentage(temp.first, temp.second) << "%" << "\n";
+        }
+    }
+    else { //code to output to file
+        std::ofstream myfile;
+        myfile.open(file_name);
+        myfile << "Assignment , Grade" << "\n";
+        for (int i = 0; i < size; i++) {
+            std::pair<int, int> temp = individual_grade(ordered_list[i]);
+            myfile << ordered_list[i] << " , " << temp.first << "/" << temp.second << "\t" << "or "
+                      << turn_to_percentage(temp.first, temp.second) << "%" << "\n";
+        }
+        myfile.close();
+    }
+}
+
+void GradeBook::printCategory(std::string file_name){
+    //looping through to print the total grade of every given category as a percentage and out of course value
+
+    if (file_name.size() == 0) { //code for printing to terminal
+        std::cout << "Category Totals:" << "\n";
+        for (int i = 0; i < unique_category.size(); i++) {
+            std::cout << unique_category[i] << " " << category_grade(unique_category[i]).first << "/" << category_grade(unique_category[i]).second
+                      << "\t" << "or " << turn_to_percentage(
+                    category_grade(unique_category[i]).first, category_grade(unique_category[i]).second) << "%" << "\n";
+        }
+    }
+    else { //code for printing to output file
+        std::ofstream myfile;
+        myfile.open(file_name);
+        myfile << "Category , Grade" << "\n";
+        for (int i = 0; i < unique_category.size(); i++) {
+            myfile << unique_category[i] << " , " << category_grade(unique_category[i]).first << "/" << category_grade(unique_category[i]).second
+                   << "\t" << "or " << turn_to_percentage(
+                    category_grade(unique_category[i]).first, category_grade(unique_category[i]).second) << "%" << "\n";
+        }
+        myfile.close();
+    }
+}
+
+void GradeBook::printOverall(std::string file_name, int condition){
+    //printing overall course grade as a percentage and out of total point
+
+    std::vector<std::string> ordered_list;
+    for (int j = 0; j < unique_category.size(); j++) { //Orders all of the given assignments by category
+        for (int i = 0; i < category.size(); i++) {
+            if (category[i] == unique_category[j]) {
+                ordered_list.push_back(name[i]);
+            }
+        }
+    }
+
+    if (file_name.size() == 0) { // code for printing to terminal
+        std::cout << "Course Total: " << course_total_grade().first << "/" << course_total_grade().second << "\t" << "or "
+                  << turn_to_percentage(course_total_grade().first, course_total_grade().second) << "%" << "\n";
+    }
+    else { // code for printing to output file
+        if (condition == 0) { //all grades condition
+            std::ofstream myfile;
+            myfile.open(file_name);
+            myfile << "Assignment , Grade" << "\n";
+            for (int i = 0; i < size; i++) {
+                std::pair<int, int> temp = individual_grade(ordered_list[i]);
+                myfile << ordered_list[i] << " , " << temp.first << "/" << temp.second << "\t" << "or "
+                       << turn_to_percentage(temp.first, temp.second) << "%" << "\n";
+            }
+            myfile << "Course Grade" << "\n";
+            myfile << course_total_grade().first << "/" << course_total_grade().second << "\t" << "or "
+                   << turn_to_percentage(course_total_grade().first, course_total_grade().second) << "%" << "\n";
+            myfile.close();
+        }
+        else if (condition == 1) { //all categories condition
+            std::ofstream myfile;
+            myfile.open(file_name);
+            myfile << "Category , Grade" << "\n";
+            for (int i = 0; i < unique_category.size(); i++) {
+                myfile << unique_category[i] << " , " << category_grade(unique_category[i]).first << "/" << category_grade(unique_category[i]).second
+                       << "\t" << "or " << turn_to_percentage(
+                        category_grade(unique_category[i]).first, category_grade(unique_category[i]).second) << "%" << "\n";
+            }
+            myfile << "Course Grade" << "\n";
+            myfile << course_total_grade().first << "/" << course_total_grade().second << "\t" << "or "
+                   << turn_to_percentage(course_total_grade().first, course_total_grade().second) << "%" << "\n";
+            myfile.close();
+        }
+        else if (condition == 2) { //only course grade condition
+            std::ofstream myfile;
+            myfile.open(file_name);
+            myfile << "Course Grade" << "\n";
+            myfile << course_total_grade().first << "/" << course_total_grade().second << "\t" << "or "
+                   << turn_to_percentage(course_total_grade().first, course_total_grade().second) << "%" << "\n";
+            myfile.close();
+        }
+    }
+}
+
 std::pair<int, int> GradeBook::individual_grade(std::string individual_name)
 {
     //we need to create an index so we can get the matching category and points_earned
@@ -77,15 +217,16 @@ std::pair<int, int> GradeBook::individual_grade(std::string individual_name)
         if (name[i] == individual_name)
         {
             index = i;
-            break;
+            individual_points_total = this->get_assignment_points_total(category[index]);
+
+            float individual_points_earned = individual_points_total*(points_earned[index]/float(100));
+
+            return std::make_pair((int(individual_points_earned)),individual_points_total);
         }
 
-        individual_points_total = this->get_assignment_points_total(category[index]);
-
-        return std::make_pair((points_earned[index]),individual_points_total);
     }
 }
-
+//adds up scores of all assignments in a certain category
 std::pair<int, int> GradeBook::category_grade(std::string individual_category)
 {
     int total_points_earned = 0;
@@ -103,33 +244,68 @@ std::pair<int, int> GradeBook::category_grade(std::string individual_category)
         total_points_earned += scores_per_category[i];
     }
     int category_points_total = this->get_assignment_points_total(individual_category);
+    float category_percent = float(total_points_earned)/float((100*scores_per_category.size())); //turning points earned in a category into percentage
     category_points_total*= scores_per_category.size(); //making sure the total points cover every project of that category;
+    //std::cout << category_percent << "\n";
+    int category_points = category_percent*float(category_points_total); //turning that percentage into points earned out of category total
 
-    return std::make_pair(total_points_earned,category_points_total);
+    return std::make_pair(category_points,category_points_total);
 }
 
-//will essentially run the sum of category_grade for every category. Perhaps I should make
+//will essentially run the sum of category_grade for every category
 std::pair<int, int> GradeBook::course_total_grade()
 {
-    const char* category_list[] = {"Lab", "Assignment", "Project1", "Project2", "Exam"};
+    //const char* category_list[] = {"Lab", "Assignment", "Project1", "Project2", "Exam"};
     std::pair<int, int> total_course_score; //first is points_earned, and second is points_total
     std::pair<int, int> individual_category_score;
     total_course_score.first = 0;
     total_course_score.second = 0;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < unique_category.size(); i++)
     {
-        individual_category_score = category_grade(category_list[i]);
+        individual_category_score = category_grade(unique_category[i]);
         total_course_score.first += individual_category_score.first;
         total_course_score.second += individual_category_score.second;
     }
     return total_course_score;
 
 }
-//turns a set of points_earned and points_total into a percentage;
-int GradeBook::turn_to_percentage(int grade_points_earned, int grade_points_total)
+//turns a set of points_earned and points_total into a percentage
+float GradeBook::turn_to_percentage(int grade_points_earned, int grade_points_total)
 {
-    float ratio = grade_points_earned / grade_points_total;
-    ratio *= 100;
-    int grade = (int) ratio;
-    return grade;
+    float ratio = float(grade_points_earned) / float(grade_points_total);
+    ratio *= float(100);
+    //float grade = (int) ratio;
+    return ratio;
+}
+//changes the grade of a given assignment to the given grade
+void GradeBook::change_grade(std::string assignment_name, int new_grade) {
+    for (int i = 0; i < name.size(); i++) {
+        if (name[i] == assignment_name) {
+            this->points_earned[i] = new_grade;
+            break;
+        }
+    }
+}
+//prints out the whole gradebook to a file
+void GradeBook::printGradeBook(std::string file_name) {
+    std::ofstream myfile;
+    myfile.open(file_name);
+    myfile << "Assignment,Category,Grade" << "\n";
+    for (int i = 0; i < name.size(); i++) {
+        myfile << name[i] << "," << category[i] << "," << points_earned[i] << "\n";
+    }
+}
+//prints out individual assignment grade to either terminal or file
+void GradeBook::printIndividualAssignment(std::string file_name, std::string assign_name) {
+    if (file_name.size() == 0) { //to terminal
+        std::cout << assign_name << " " << individual_grade(assign_name).first << "/" << individual_grade(assign_name).second << "\t" << "or " << turn_to_percentage(
+                individual_grade(assign_name).first, individual_grade(assign_name).second) << "%" << "\n";
+    }
+    else { // to file
+        std::ofstream myfile;
+        myfile.open(file_name);
+        myfile << assign_name << " " << individual_grade(assign_name).first << "/" << individual_grade(assign_name).second << "\t" << "or " << turn_to_percentage(
+                individual_grade(assign_name).first, individual_grade(assign_name).second) << "%" << "\n";
+        myfile.close();
+    }
 }
