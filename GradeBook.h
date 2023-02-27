b6da6ca22eb35df57da61ca47c9caa0cb416c404
ivanabreu0 <102328381+ivanabreu0@users.noreply.github.com>
2023-02-27 14:12:07 -0500

    Update GradeBook.h

--- GradeBook.h
+++ GradeBook.h
@@ -11,18 +11,25 @@ private:
     std::vector<std::string> category;
     std::vector<int> points_earned;
     std::vector<std::string> name;
+    int size;
+    std::vector<std::string> unique_category;
 
 public:
     //default constructor
-    GradeBook::GradeBook();
+    GradeBook();
     //constructor
     GradeBook (std::vector<std::string> category_vector, std::vector<std::string> name_vector, std::vector<int> points_earned_vector);
+    //orders categories into unique categories
+    void order_category();
 
     //methods that allow you to append a value to one of the three vectors
     void category_add_on(std::string value);
     void name_add_on(std::string value);
     void points_earned_add_on(int value);
 
+    void printAssignment(std::string file_name);
+    void printCategory(std::string file_name);
+    void printOverall(std::string file_name, int condition);
     //for each of these, these return the points_earned and points_total.
     //The reason why is because I can reuse code.
     //we will do this using a pair<int><int>. First will be points_earned and second will be points_total
@@ -30,10 +37,16 @@ public:
     std::pair<int, int> category_grade(std::string individual_category);
     std::pair<int, int> course_total_grade();
     //turns a set of points_earend and points_total into a percentage;
-    int turn_to_percentage(int grade_points_earned, int grade_points_total);
+    float turn_to_percentage(int grade_points_earned, int grade_points_total);
 
     //grabs the point total of a specific category
     int get_assignment_points_total(std::string category_name);
+    //allows user to change the grade of a specific assignment
+    void change_grade(std::string assignment_name, int new_grade);
+    //prints out the gradebook in the original csv file format
+    void printGradeBook(std::string file_name);
+    //prints out one chosen assignment
+    void printIndividualAssignment(std::string file_name, std::string assign_name);
 
 };
 
